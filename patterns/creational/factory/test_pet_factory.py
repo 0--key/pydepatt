002a24401c41106cffaf73239ff921eab5b5ef29
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-03-09 22:34:39 +0200

    Test suite extension

--- patterns/creational/factory/test_pet_factory.py
+++ patterns/creational/factory/test_pet_factory.py
@@ -1,4 +1,9 @@
 import unittest
 from pet_factory import Dog, Cat, spawn_pet
 
-print("Import works!")
+class TestPetBehavior(unittest.TestCase):
+
+    def test_dog_properties(self):
+        D = Dog('Hound')
+        # self.assertRaises(AttributeError, lambda: D._name)
+        self.assertEqual(D._name, 'Hound')
