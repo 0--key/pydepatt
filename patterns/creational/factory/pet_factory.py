b932e0b28f85e3e57c939bd85ba768e164517693
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-03-09 22:16:11 +0200

    Pet factory as a factory sample

--- patterns/creational/factory/pet_factory.py
+++ patterns/creational/factory/pet_factory.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This is a sample tangled content
+"""
+class Dog(object):
+    """A simple dog class"""
 
-print("This is Org #include test message")
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Woof!"
+
+
+class Cat(object):
+    """A simple cat class"""
+
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Meow!"
+
+
+def spawn_pet(pet="dog"):
+    """The factory method"""
+    pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+    return pets[pet]
+
+d = spawn_pet("dog")
+print(d.speak())
+c = spawn_pet("cat")
+print(c.speak())
