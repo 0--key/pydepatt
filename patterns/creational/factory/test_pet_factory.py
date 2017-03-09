b89778553a5157c3a5db8985d767e44e00f9dcb9
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-03-09 18:59:34 +0200

    Pet factory test suite extension

--- patterns/creational/factory/test_pet_factory.py
+++ patterns/creational/factory/test_pet_factory.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+An attempt to illustrate how pet_factory works
+"""
 import unittest
 from pet_factory import Dog, Cat, spawn_pet
 
+
 class TestPetBehavior(unittest.TestCase):
 
-    def test_dog_properties(self):
-        D = Dog('Hound')
-        # self.assertRaises(AttributeError, lambda: D._name)
-        self.assertEqual(D._name, 'Hound')
+    def setUp(self):
+        self.D = Dog('Hound')
+
+    def test_dog_init_name(self):
+        self.assertEqual(self.D._name, 'Hound')
+
+    def test_dog_speak_ability(self):
+        self.assertEqual(self.D.speak(), 'Woof!')
