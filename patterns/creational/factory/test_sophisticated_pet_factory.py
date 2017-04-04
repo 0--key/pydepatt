8f9b898479730c15cc2ee696db413a0dac7a11a0
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-04-04 15:43:49 +0200

    Test suite extension

--- patterns/creational/factory/test_sophisticated_pet_factory.py
+++ patterns/creational/factory/test_sophisticated_pet_factory.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-An attempt to illustrate how pet_factory works
+Discover how it works
 """
 import unittest
-from sophisticated_pet_factory import Dog, Cat, PetFactory
+from sophisticated_pet_factory import Dog, \
+    Cat, PetFactory
 
 
 class TestPetBehavior(unittest.TestCase):
 
     def setUp(self):
         self.pet_factory = PetFactory()
-        # self.cat = PetFactory('Cat')
+
 
     def test_dog_speak_ability(self):
-        self.assertEqual(self.pet_factory.make_clamor('Dog'), 'Bhow Bhow!!')
+        self.assertEqual(
+            self.pet_factory.make_clamor('Dog'),
+            'Bhow Bhow!!')
+
+    def test_cat_speak_ability(self):
+        self.assertEqual(
+            self.pet_factory.make_clamor('Cat'),
+            'Meow Meow!!')
+
+    def tearDown(self):
+        del self.pet_factory
