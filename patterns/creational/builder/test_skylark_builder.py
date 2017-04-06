d3f6de5105081cd5d729ac677416ba3950c91902
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-04-06 17:13:07 +0200

    Sources of pattern and test suite there

---
+++ patterns/creational/builder/test_skylark_builder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+An attempt to illustrate how it works
+"""
+import unittest
+
+
+class TestSimpleBehavior(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
